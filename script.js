68b2afed439d25651bef7e677b58d084edfe5526
sgm19788 <135103042+sgm19788@users.noreply.github.com>
2026-02-25 03:21:58 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
-<script>
-  // نصوص تظهر بشكل متتابع
-  const texts = ["مرحبا بك في Safrly", "احجز عروضك بسهولة", "أفضل تجربة حجز"];
-  let count = 0;
-  let index = 0;
-  let currentText = '';
-  let letter = '';
+const texts = ["مرحبا بك في Safrly", "احجز عروضك بسهولة", "أفضل تجربة حجز"];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
 
-  (function type() {
-    if(count === texts.length) count = 0;
-    currentText = texts[count];
-    letter = currentText.slice(0, ++index);
+(function type() {
+  if(count === texts.length) count = 0;
+  currentText = texts[count];
+  letter = currentText.slice(0, ++index);
 
-    document.getElementById('hero-title').textContent = letter;
-    if(letter.length === currentText.length){
-      count++;
-      index = 0;
-      setTimeout(type, 1500); // وقت الانتظار قبل النص التالي
-    } else {
-      setTimeout(type, 150);
-    }
-  }());
-</script>
+  document.getElementById('hero-title').textContent = letter;
+  if(letter.length === currentText.length){
+    count++;
+    index = 0;
+    setTimeout(type, 1500);
+  } else {
+    setTimeout(type, 150);
+  }
+}());
