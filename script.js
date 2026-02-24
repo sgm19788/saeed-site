9ee55a3297510f3afc0101cdf0256bc717429a5e
sgm19788 <135103042+sgm19788@users.noreply.github.com>
2026-02-25 03:08:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
+<script>
+  // نصوص تظهر بشكل متتابع
+  const texts = ["مرحبا بك في Safrly", "احجز عروضك بسهولة", "أفضل تجربة حجز"];
+  let count = 0;
+  let index = 0;
+  let currentText = '';
+  let letter = '';
 
-document.getElementById("root").innerHTML = "<h1>مرحبا بكم في موقع سعيد الهاجري</h1>";
+  (function type() {
+    if(count === texts.length) count = 0;
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById('hero-title').textContent = letter;
+    if(letter.length === currentText.length){
+      count++;
+      index = 0;
+      setTimeout(type, 1500); // وقت الانتظار قبل النص التالي
+    } else {
+      setTimeout(type, 150);
+    }
+  }());
+</script>
